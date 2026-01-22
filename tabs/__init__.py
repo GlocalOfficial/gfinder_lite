7b80622cb2632521dcc969bbc8171ccd92ac03b1
gfinderdata-rgb <gfinder.data@gmail.com>
2026-01-22 13:41:59 +0900

    test

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -8,8 +8,8 @@ from .latest_tab import render_latest_tab
 from .summary_tab import render_summary_tab
 
 __all__ = [
-    "render_counts_tab",
     "render_results_tab",
+    "render_counts_tab",
     "render_latest_tab",
     "render_summary_tab",
 ]
